6e57528ed7357ffdf6a7eb70adaef135cbf9d3a5
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-12-24 17:04:51 -0600

    Add solution to "Transcribing DNA into RNA"

---
+++ R/rna.R
@@ -0,0 +1,20 @@
+#' Transcribing DNA into RNA
+#' 
+#' An RNA string is a string formed from the alphabet containing 'A',
+#' 'C', 'G', and 'U'.
+#'
+#' Given a DNA string t corresponding to a coding strand, its
+#' transcribed RNA string u is formed by replacing all occurrences of
+#' 'T' in t with 'U' in u.
+#' 
+#' Given: A DNA string t having length at most 1000 nt.
+#' 
+#' Return: The transcribed RNA string of t.
+
+library(magrittr)
+
+f <- 'data/rosalind_rna.txt'
+
+readLines(f) %>%
+  gsub('T', 'U', .) %>%
+  cat()
